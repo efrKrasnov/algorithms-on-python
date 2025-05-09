23b6783312f3205f6387d2bf34f5336f54b07c95
v.krasnov <vkrasnov@cism-ms.ru>
2025-05-09 10:41:43 +0300

    make some optimizations add comments

--- chapter-1/two_tournamen/test_two_tournament.py
+++ chapter-1/two_tournamen/test_two_tournament.py
@@ -1,29 +1,29 @@
 from random import shuffle
-from tournament_two import tournament_two
+from tournament_two import max_tournament_algo
 
 def test_sorted_with_odd_len() -> None:
     arr: list[int] = [i for i in range(1, 10)]
-    assert tournament_two(arr) == 9
+    assert max_tournament_algo(arr) == 9
     
 def test_shuffled_with_odd_len() -> None:
     arr: list[int] = [i for i in range(1, 10)]
     shuffle(arr)
-    assert tournament_two(arr) == 9 
+    assert max_tournament_algo(arr) == 9 
     
     
 def test_sorted_with_even_len() -> None:
     arr: list[int] = [i for i in range(1, 11)]
-    assert tournament_two(arr) == 10
+    assert max_tournament_algo(arr) == 10
     
 def test_shuffled_with_even_len() -> None:
     arr: list[int] = [i for i in range(1, 11)]
     shuffle(arr)
-    assert tournament_two(arr) == 10
+    assert max_tournament_algo(arr) == 10
     
     
 def test_with_single_item() -> None:
-    assert tournament_two([10]) == 10 
+    assert max_tournament_algo([10]) == 10 
     
     
 def test_empty() -> None:
-    assert tournament_two([]) is None
+    assert max_tournament_algo([]) is None
